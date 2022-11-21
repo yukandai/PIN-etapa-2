62730e32be9e19b2f482a22c9ffda8e15184d55d
Marcelo Guazzardo <tercermundo19@gmail.com>
2022-11-21 18:13:25 -0300

    .

--- setup.tf
+++ setup.tf
@@ -2,11 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-#Create key-pair for logging into EC2 in us-east-1
-resource "aws_key_pair" "webserver-key" {
-  key_name   = "webserver-key"
-  public_key = file("~/.ssh/id_rsa.pub")
-}
 
 
 #Get Linux AMI ID using SSM Parameter endpoint in us-east-1
