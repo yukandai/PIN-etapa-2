e82d1de4ea359be02e44ad9e58658df354349c64
Marcelo Guazzardo <tercermundo19@gmail.com>
2022-11-21 18:11:28 -0300

    .

--- main.tf
+++ main.tf
@@ -6,19 +6,6 @@ resource "aws_instance" "webserver" {
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.sg.id]
   subnet_id                   = aws_subnet.subnet.id
-  provisioner "remote-exec" {
-    inline = [
-      "sudo yum -y install httpd && sudo systemctl start httpd",
-      "echo '<h1><center>My Test Website With Help From Terraform Provisioner</center></h1>' > index.html",
-      "sudo mv index.html /var/www/html/"
-    ]
-    connection {
-      type        = "ssh"
-      user        = "ec2-user"
-      private_key = file("~/.ssh/id_rsa")
-      host        = self.public_ip
-    }
-  }
   tags = {
     Name = "webserver"
   }
