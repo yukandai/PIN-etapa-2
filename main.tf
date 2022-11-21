4f0faf7adf7e05f28a40d6617cb162b47fe6d36b
Marcelo Guazzardo <tercermundo19@gmail.com>
2022-11-21 18:17:37 -0300

    .

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "webserver" {
   ami                         = data.aws_ssm_parameter.webserver-ami.value
   instance_type               = "t3.micro"
-  key_name                    = app-ssh-key 
+  #key_name                    = app-ssh-key 
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.sg.id]
   subnet_id                   = aws_subnet.subnet.id
