5ab602d42c43a7bb37a21827708a47398e6b0989
tercemundo <94326261+tercemundo@users.noreply.github.com>
2023-03-27 21:43:09 -0300

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3"{
-    bucket                 = "mundose22"
-    region                 = "us-east-1"
-    key                    = "backend.tfstate"
-    dynamodb_table         = "terraformstatelock"
-  }
-}
