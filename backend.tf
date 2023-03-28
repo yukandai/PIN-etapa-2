bfd83326e9fba79a7378ec5f021cc1aa153d1278
tercemundo19@gmail.com <tercemundo19@gmail.com>
2023-03-28 19:22:21 -0300

    adding backend

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3"{
-    bucket                 = "mundose22"
+    bucket                 = "mundose2"
     region                 = "us-east-1"
     key                    = "backend.tfstate"
     dynamodb_table         = "terraformstatelock"
