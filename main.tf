6d811bc9404880672122ec5969fdc06518ab536b
tercemundo <94326261+tercemundo@users.noreply.github.com>
2023-03-29 19:30:53 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 ##Create and bootstrap webserver #lanzar
 resource "aws_instance" "webserver" {
   ami                         = "ami-00c39f71452c08778"
-  instance_type               = "t3.micro"
+  instance_type               = "t2.micro"
   #key_name                    = app-ssh-key 
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.sg.id]
