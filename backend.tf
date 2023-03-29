ee2b08f1885c9d2e04657b549418d6d70a594530
tercemundo <94326261+tercemundo@users.noreply.github.com>
2023-03-29 18:21:46 -0300

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3"{
-    bucket                 = "mundose2"
+    bucket                 = "mundose22"
     region                 = "us-east-1"
     key                    = "backend.tfstate"
     dynamodb_table         = "terraformstatelock"
