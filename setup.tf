c89caf99dc316066b2a5e069254631ddeaa71fc5
tercemundo <94326261+tercemundo@users.noreply.github.com>
2023-03-29 19:31:54 -0300

    Update setup.tf

--- setup.tf
+++ setup.tf
@@ -5,9 +5,9 @@ provider "aws" {
 
 
 #Get Linux AMI ID using SSM Parameter endpoint in us-east-1
-data "aws_ssm_parameter" "webserver-ami" {
-  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
-}
+#data "aws_ssm_parameter" "webserver-ami" {
+#  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+#}
 
 #Create VPC in us-east-1
 resource "aws_vpc" "vpc" {
