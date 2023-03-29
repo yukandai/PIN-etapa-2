632b0547f64e8c93f676ba5fb57dbb2b05fba30a
tercemundo <94326261+tercemundo@users.noreply.github.com>
2023-03-29 18:51:10 -0300

    Update setup.tf

--- setup.tf
+++ setup.tf
@@ -2,6 +2,8 @@ provider "aws" {
   region = "us-east-1"
 }
 
+
+
 #Get Linux AMI ID using SSM Parameter endpoint in us-east-1
 data "aws_ssm_parameter" "webserver-ami" {
   name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
