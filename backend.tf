a8b3a6715b5ed71e61f0459ef8c8ebfd64ae7761
cintia <cintia.ossandon@gmail.com>
2024-08-06 21:16:51 -0300

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3"{
-    bucket                 = "mundose22"
+    bucket                 = "mi-bucket-de-ejemplo"
     region                 = "us-east-1"
     key                    = "backend.tfstate"
     dynamodb_table         = "terraformstatelock"
